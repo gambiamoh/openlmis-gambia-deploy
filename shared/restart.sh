ec03ef74a3b86d67e1f59ee1e592ddb2473d5192
Paweł Gesek <pgesek@soldevelo.com>
2024-07-19 14:47:57 +0200

    Update to use new compose

--- shared/restart.sh
+++ shared/restart.sh
@@ -3,16 +3,16 @@
 WIPE_MSG="Will WIPE data!"
 KEEP_MSG="Will keep data."
 
-docker-compose kill
-docker-compose down
+docker compose kill
+docker compose down
 
 export KEEP_OR_WIPE="keep"
 if [ "$KEEP_OR_WIPE" == "wipe" ]; then
   echo "$WIPE_MSG"
   unset spring_profiles_active
-  docker-compose up --build --force-recreate -d
+  docker compose up --build --force-recreate -d
 else
   echo "$KEEP_MSG";
   export spring_profiles_active="production"
-  docker-compose up --build --force-recreate -d
+  docker compose up --build --force-recreate -d
 fi
