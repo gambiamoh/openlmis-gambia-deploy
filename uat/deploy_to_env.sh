6548ed7202bbc4dc5f382285183094d5f0c0da9e
Paweł Gesek <pgesek@soldevelo.com>
2024-07-19 11:27:17 +0200

    Enable remote Docker in deploy script

--- uat/deploy_to_env.sh
+++ uat/deploy_to_env.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-#export DOCKER_TLS_VERIFY="1"
-#export DOCKER_HOST="ssh://ubuntu@gambia.3.elmis-dev.org"
-#export DOCKER_CERT_PATH="${PWD}/credentials"
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="dev.elmis.apps.moh.gm:2376"
+export DOCKER_CERT_PATH="${PWD}/credentials"
 export KEEP_OR_WIPE="keep"
 
 #../shared/init_env_gh.sh
