fbfee36ce4474ed66b4e745175cc249f9d779270
user <kpavlenko@soldevelo.com>
2024-08-19 10:25:11 +0200

    added deploy_services script

---
+++ dev/reporting/services/deploy_services.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+export DOCKER_TLS_VERIFY="1"
+export COMPOSE_TLS_VERSION=TLSv1_2
+export DOCKER_HOST="dev.elmis.report.apps.moh.gm:2376"
+export DOCKER_CERT_PATH="${PWD}/../../../credentials"
+export DOCKER_COMPOSE_BIN=/usr/local/bin/docker-compose
+
+export REPORTING_DIR_NAME=reporting
+
+reportingRepo=$1
+
+# To avoid running the whole ETL process, we need to create a volume for
+# Postgres data and mark it as external, so that Nifi can update already
+# persisted data.
+docker volume create pgdata
+
+cd "$reportingRepo/$REPORTING_DIR_NAME"
+$DOCKER_COMPOSE_BIN kill
+$DOCKER_COMPOSE_BIN down -v --remove-orphans
+$DOCKER_COMPOSE_BIN up --build --force-recreate -d
