447d6cb46fa10973ebbe42e642454ef572d582ba
user <kpavlenko@soldevelo.com>
2024-08-19 10:30:44 +0200

    updated the path to docker-compose

--- prod/reporting/services/deploy_services.sh
+++ prod/reporting/services/deploy_services.sh
@@ -4,7 +4,7 @@ export DOCKER_TLS_VERIFY="1"
 export COMPOSE_TLS_VERSION=TLSv1_2
 export DOCKER_HOST="elmis.report.apps.moh.gm:2376"
 export DOCKER_CERT_PATH="${PWD}/../../../credentials"
-export DOCKER_COMPOSE_BIN=/usr/local/bin/docker-compose
+export DOCKER_COMPOSE_BIN=/usr/bin/docker-compose
 
 export REPORTING_DIR_NAME=reporting
 
