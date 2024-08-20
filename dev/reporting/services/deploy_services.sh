e5303db126bae4041688c27643a84b843edb73c7
user <kpavlenko@soldevelo.com>
2024-08-20 09:30:18 +0200

    updated path for docker certs

--- dev/reporting/services/deploy_services.sh
+++ dev/reporting/services/deploy_services.sh
@@ -3,7 +3,7 @@
 export DOCKER_TLS_VERIFY="1"
 export COMPOSE_TLS_VERSION=TLSv1_2
 export DOCKER_HOST="dev.elmis.report.apps.moh.gm:2376"
-export DOCKER_CERT_PATH="${PWD}/../../../deployment-config/dev_env/credentials"
+export DOCKER_CERT_PATH="${PWD}/../../../deployment-config/dev_env/credentials/superset_credentials"
 export DOCKER_COMPOSE_BIN=/usr/bin/docker-compose
 
 export REPORTING_DIR_NAME=reporting
