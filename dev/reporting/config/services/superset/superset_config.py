039db0452e106a3d452954668c8dd8fdd552e03d
Piotr Wargulak <pwargulak@soldevelo.com>
2024-08-21 14:43:42 +0200

    Added more Superset Reporting config based on Core OLMIS

--- dev/reporting/config/services/superset/superset_config.py
+++ dev/reporting/config/services/superset/superset_config.py
@@ -47,10 +47,14 @@ OAUTH_PROVIDERS = [
             },
             'api_base_url': '%s/api/oauth' % os.environ['OL_BASE_URL'],
             'access_token_url': '%s/api/oauth/token?grant_type=authorization_code' % os.environ['OL_BASE_URL'],
-            'authorize_url': '%s/api/oauth/authorize?' % os.environ['OL_BASE_URL']
+            'authorize_url': '%s/api/oauth/authorize?' % os.environ['OL_BASE_URL'],
+            'custom_redirect_url': '%s/oauth-authorized/openlmis?' % os.environ['SUPERSET_URL']
         }
     }
 ]
+
+# Set URL scheme for superset-patchup redirect URLs
+PREFERRED_URL_SCHEME = "https"
 
 # The default user self registration role
 AUTH_USER_REGISTRATION_ROLE = "OLMIS_Gamma"
@@ -84,5 +88,6 @@ CUSTOM_ROLES = {'OLMIS_Gamma': {'all_datasource_access'}}
 
 # Enable template processing in SQL - among others for {{current_username()}} macro
 FEATURE_FLAGS = {
-    "ENABLE_TEMPLATE_PROCESSING": True
+    "ENABLE_TEMPLATE_PROCESSING": True,
+    "VERSIONED_EXPORT": True
 }
